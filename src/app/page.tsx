3b8faf180190f4e56210d9cb02113ccf7351838e
afrizaharahap <Afriza.harahap@gmail.com>
2024-10-18 16:19:12 +0700

    preparing-ui-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="flex gap-4">
+      <Input type="text" placeholder="Input" />
       <Button>Primary</Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="destructive">Destructive</Button>
       <Button variant="ghost">Ghost</Button>
-      <Button variant="link">Link</Button>
+      <Button variant="muted">Muted</Button>
       <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
