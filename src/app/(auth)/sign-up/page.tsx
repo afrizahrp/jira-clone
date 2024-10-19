a643548babe8cdd15c7cf5d4ea1898036e46844b
AFRIZAHRP <afriza.hrp@gmail.com>
2024-10-20 01:46:06 +0700

    starting-auth-page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const SignUpPage = () => {
+    return (
+        <div >
+            Signup Page
+        </div>
+    )
+}
+export default SignUpPage
--- src/features/component.tsx
+++
@@ -1,5 +0,0 @@
-const Test = () => {
-  return <div className='text-red-500 '>Hello Test Component</div>;
-};
-
-export default Test;
